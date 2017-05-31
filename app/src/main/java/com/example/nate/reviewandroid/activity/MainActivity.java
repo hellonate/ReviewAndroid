3c6a620db0290912808796fa302b56dc1e397824
hellonate <821366930@qq.com>
2017-05-31 18:04:56 +0800

    修改了进入的bug

--- app/src/main/java/com/example/nate/reviewandroid/activity/MainActivity.java
+++ app/src/main/java/com/example/nate/reviewandroid/activity/MainActivity.java
@@ -66,38 +66,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 }
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        Log.d(TAG,"onStart");
-//    }
-//
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        Log.d(TAG,"onStop");
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        Log.d(TAG,"onDestroy");
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//          Log.d(TAG,"onPause");
-//    }
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        Log.d(TAG,"onResume");
-//    }
-//
-//    @Override
-//    protected void onRestart() {
-//        super.onRestart();
-//        Log.d(TAG,"onRestart");
-//    }
+
--- app/src/main/java/com/example/nate/reviewandroid/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/nate/reviewandroid/activity/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.nate.reviewandroid.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,34 +16,56 @@ import com.example.nate.reviewandroid.R;
 public class WelcomeActivity extends AppCompatActivity {
     private ImageView imageView;
     private Button btn_skip;
-    private boolean isFirstIn = true;
     private String TAG = "WelcomeActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        if (isFirstIn){
-            imageView = (ImageView) findViewById(R.id.imageview);
-            Animation animation = AnimationUtils.loadAnimation(this, R.anim.animation);
-            imageView.startAnimation(animation);
-            btn_skip = (Button) findViewById(R.id.btn_skip);
-            btn_skip.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        SharedPreferences sp = getSharedPreferences("Text",MODE_PRIVATE);
+        boolean isCome = sp.getBoolean("isCome",false);
+
+        if (isCome){
+            ////如果用户不是第一次进来
+
                     Intent intent = new Intent(getBaseContext(), MainActivity.class);
                     startActivity(intent);
                     finish();
-                }
-            });
-            isFirstIn = false;
-            Log.e(TAG,isFirstIn + "");
 
-        }else{
-            Intent intent = new Intent(getBaseContext(), MainActivity.class);
-            startActivity(intent);
-            finish();
+
+
         }
+        //如果没有来过
+        imageView = (ImageView) findViewById(R.id.imageview);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.animation);
+        imageView.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                save();
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
 
+    }
+
+
+    public void save(){
+        SharedPreferences sp = getSharedPreferences("Text",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isCome",true);
+        editor.commit();
     }
 }
